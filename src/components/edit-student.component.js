fed4e9e68d7c10406cc8f82ad3af74e50eb36b84
fatihahmetg <fatihahmet06@hotmail.com>
2023-04-26 13:31:53 -0400

    added heroku links to route the data

--- src/components/edit-student.component.js
+++ src/components/edit-student.component.js
@@ -23,7 +23,7 @@ export default class EditStudent extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/students/edit-student/' + this.props.match.params.id)
+    axios.get('https://job-platform-fxg147.herokuapp.com/students/edit-student/' + this.props.match.params.id)
       .then(res => {
         this.setState({
           name: res.data.name,
@@ -57,7 +57,7 @@ export default class EditStudent extends Component {
       rollno: this.state.rollno
     };
 
-    axios.put('http://localhost:4000/students/update-student/' + this.props.match.params.id, studentObject)
+    axios.put('https://job-platform-fxg147.herokuapp.com/students/update-student/' + this.props.match.params.id, studentObject)
       .then((res) => {
         console.log(res.data)
         console.log('Student successfully updated')
