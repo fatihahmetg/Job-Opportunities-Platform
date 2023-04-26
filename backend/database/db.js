22c714bce3d96d21d19cc8287733e230147163dd
fatihahmetg <fatihahmet06@hotmail.com>
2023-04-26 12:59:01 -0400

    added connection to cloud database
    Also changed structure

---
+++ backend/database/db.js
@@ -0,0 +1,8 @@
+/* module.exports = {
+  db: 'mongodb://localhost:27017/reactdb',
+}
+
+ */
+module.exports = {
+  db: 'mongodb+srv://fxg147:sY5LKjFJOWfAtRC1@jobapp.xygn53x.mongodb.net/jobappdatabase?retryWrites=true&w=majority',
+}
